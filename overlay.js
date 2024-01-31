044047f816d86d0ff096a389f03c2df00374c378
ZENATOZENA1 <kenzenamahendraputra@gmail.com>
2024-01-31 21:44:05 +0700

    update

--- overlay.js
+++ overlay.js
@@ -1,3 +1,48 @@
 window.onload = function() {
-    document.getElementById("header00").innerHTML = '<div><h1>OSKAMA 12</h1> </div><div><h4>OSIS TARUNA TARUNI ANGKATAN 12</h4></div>';
+    // O S K A M A  1 2
+    // OSIS TARUNA TARUNI ANGKATAN 12
+    document.getElementById("header00").innerHTML = 
+    '<div>'+
+        '<h1>OSKAMA 12</h1>'+
+   ' </div>'+
+   ' <div>'+
+       ' <h4>OSIS TARUNA TARUNI ANGKATAN 12</h4>'+
+    '</div>';
+
+    // BERANDA | ACARA | GALERI | MATERI
+    document.getElementById("header").innerHTML =
+    '<div class="a01">'+
+        '<div class="a02">'+
+            '<div class="l01" id="beranda">'+
+                '<a href="https://kzmpgh.github.io/oskama12"><h4>BERANDA</h4></a>'+
+            '</div>'+
+            '<div class="l01" id="acara">'+
+                '<a href="https://kzmpgh.github.io/oskama12/event/"><h4>ACARA</h4></a>'+
+            '</div>'+
+            '<div class="l01" id="galeri">'+
+                '<a href="https://kzmpgh.github.io/oskama12/galeri/"><h4>GALERI</h4></a>'+
+            '</div>'+  
+            '<div class="l01" id="materi">'+
+                '<a href="https://kzmpgh.github.io/oskama12/materi/"><h4>MATERI</h4></a>'+
+            '</div>'+
+            '<div class="a03"></div>'+
+        '</div>'+
+    '</div>';
+
+    // Footer
+    document.getElementById("footer").innerHTML =
+    '<div class="f01">'+
+        '<div class="f02">'+
+            '<h4>Kontak</h4>'+
+            '<a href="https://api.whatsapp.com/send/?phone=6289696643899&text=Selamat%20pagi%2C+sebelumnya+mohon+maaf+menggangu+waktunya%20saya%20ingin..."><h5>Whatsapp</h5></a>'+
+            '<a href="https://www.instagram.com/kenzena_/"><h5>Instragram</h5></a>'+
+        '</div>'+
+        '<div class="f03"></div>'+
+        '<div class="bf01">'+
+            '<h5>Website</h5>'+
+            '<h3>OSKAMA 12 </h3>'+
+            '<h5>dari</h5>'+
+            '<a href="https://kzmpgh.github.io/KZMP/"><h5>KZMP</h5></a>'+
+        '</div>'+
+    '</div>';
 }
