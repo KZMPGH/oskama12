8e20f8efe6d7df0ad83aab8c924640f0fa317a73
ZENATOZENA1 <kenzenamahendraputra@gmail.com>
2024-03-27 08:43:18 +0700

    Update index.js

--- galeri/index.js
+++ galeri/index.js
@@ -56,7 +56,7 @@ function foto() {
         // Muat gambar dalam batch saat halaman di-scroll
         for (var i = index; i < end; i++) {
             var img = document.createElement("img");
-            img.src = "../../image/galeri/foto/" + images[i]; // Ganti dengan path folder gambar Anda
+            img.src = "../image/galeri/foto/" + images[i]; // Ganti dengan path folder gambar Anda
             img.loading = "lazy"; // Tambahkan atribut loading="lazy"
             img.alt = "- Error 303";
             img.onclick = function() {
